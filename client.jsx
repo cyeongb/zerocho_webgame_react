8a760bd9dc56ceede33bedffb429a7b84e401ae6
yeongbi choe <cyeongb@gmail.com>
2021-03-12 23:20:51 +0900

    2021-03-12 : gitignore fix

--- client.jsx
+++ client.jsx
@@ -1,5 +1,5 @@
 const React = require("react");
-const ReactDom = require("react-com");
+const ReactDom = require("react-dom");
 
 const WordRelay = require("./WordRelay");
 ReactDom.render(<WordRelay />, document.querySelector("#root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
-const WordRelay = require("./WordRelay");
+//const WordRelay = require("./WordRelay");
 const path = require("path");
 
 module.exports = {
-  name: "word-relay set",
+  name: "word-relay-set",
   mode: "development", //production
   devtool: "eval", //빠르게
   resolve: {
-    extensions: [".jsx"], //확장자가 .jsx로 끝나는 파일을 다 스캔한다.
+    extensions: [".jsx", ".js"], //확장자가 .jsx로 끝나는 파일을 다 스캔한다.
   },
   entry: {
     //입력
 
     //두 jsx파일을 합쳐서 하나의 js 파일로 만들 예정.
     app: ["./client", " ./WordRelay"],
+  },
+
+  module: {
+    //modules는 entry파일을 해당 module에 적용한 후에 output으로 내보냄.
+    rules: [
+      {
+        //module에 여러개의 규칙을 적용합니다.
+        test: /\.jsx?/, //정규표현식으로, .jsx파일에 rule을 적용.
+        loader: "babel-loader", //babel-loader를 적용해서 최신문법을 babel에 돌아가는 버전으로 바꿈
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-react", //preset이름으로 다운받은 것들을 넣어줌
+          ],
+        },
+      },
+    ],
   },
   output: {
     //출력
