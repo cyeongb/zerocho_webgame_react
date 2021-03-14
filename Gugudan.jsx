38710c76120d4fe3f1d8ae72408fc5b478bb1c01
yeongbi choe <cyeongb@gmail.com>
2021-03-14 20:42:52 +0900

    2021-03-14 webpack

---
+++ Gugudan.jsx
@@ -0,0 +1,58 @@
+const React = require("react");
+const { useState, useRef } = React;
+
+const Gugudan = () => {
+  const [firstNum, setFirstNum] = useState(Math.ceil(Math.random() * 9));
+  const [secondNum, setSecondNum] = useState(Math.ceil(Math.random() * 9));
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const [count, setCount] = useState(0);
+  const ref = useRef(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (parseInt(input) === firstNum * secondNum) {
+      setResult("ANSWER:" + input);
+      setFirstNum(Math.ceil(Math.random() * 9));
+      setSecondNum(Math.ceil(Math.random() * 9));
+      setInput("");
+      setCount(count + 1);
+      ref.current.focus();
+    } else {
+      setResult("WRONG!");
+      ref.current.focus();
+    }
+  };
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <>
+      <h2>Gu gu dan Question</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          {firstNum} X {secondNum} =
+          <input
+            ref={ref}
+            type="number"
+            placeholder="answer"
+            value={input}
+            onChange={onChange}
+          />
+        </div>
+
+        <button onClick={onSubmit}>click</button>
+      </form>
+      <div>CORRECT COUNT :{count}</div>
+      <div className="rs">
+        <b>{result}</b>
+      </div>
+    </>
+  );
+};
+
+module.exports = Gugudan;
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   name: "word-relay-set",
   mode: "development", //production
-  devtool: "eval", //빠르게
+  devtool: "eval", //:개발용  hidden-source-map : production용
   resolve: {
     extensions: [".jsx", ".js"], //확장자가 .jsx로 끝나는 파일을 다 스캔한다.
   },
@@ -20,12 +20,19 @@ module.exports = {
     rules: [
       {
         //module에 여러개의 규칙을 적용합니다.
-        test: /\.jsx?/, //정규표현식으로, .jsx파일에 rule을 적용.
+        test: /\.jsx$/, //정규표현식으로, .jsx파일에 rule을 적용.
         loader: "babel-loader", //babel-loader를 적용해서 최신문법을 babel에 돌아가는 버전으로 바꿈
         options: {
+          //바벨은 presets와 plugin이 없으면 아무런 동작을 하지않는다고함
           presets: [
-            "@babel/preset-env",
-            "@babel/preset-react", //preset이름으로 다운받은 것들을 넣어줌
+            // presets은 plugin의 모음이다. 그래서 presets를 좀 더 새밀하게 설정할 수 있음.
+            // 배열형태로 만들어서 첫번째 인자로는 preset이름을, 두번째 인자로는 설정값을 넣어준다.
+            [
+              "@babel/preset-env", //preset-env : 환경에맞게 맞춰줌
+              //--> 자신의 환경에 맞게 지원하는 버전만 해주는것이 좋다. 너무 많이 지원하게되면 바벨이 느려짐
+              { targets: { browsers: ["last 2 chrome versions"] } }, //최근 두 크롬 버전 브라우저만 지원한다.
+            ],
+            "@babel/preset-react", //preset-react : react를 지원하는 플러그인들
           ],
           plugins: [
             "@babel/plugin-syntax-class-properties",
