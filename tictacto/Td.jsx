f6390c66f46f363423983d80e2a2b37d8b4637e9
yeongbi choe <cyeongb@gmail.com>
2021-03-26 00:04:19 +0900

    2021-03-26 tictacto rd,tr,table add

---
+++ tictacto/Td.jsx
@@ -0,0 +1,18 @@
+import React, { useCallback, memo } from "react";
+import { CLICK_CELL } from "./Tictacto_clss";
+const Td = memo(({ rowIndex, cellIndex, dispatch, cellData }) => {
+  console.log("Td class rendering");
+
+  const onClickTd = useCallback(() => {
+    console.log("rowIndex :" + rowIndex + " cellIndex :" + cellIndex);
+
+    if (cellData) {
+      return;
+    }
+
+    dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
+  }, [cellData]);
+  return <td onClick={onClickTd}>{cellData}</td>;
+});
+
+export default Td;
---
+++ tictacto/Ttictacto_clss.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./tictacto.css";
+class Tictacto_clss extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default Tictacto_clss;
